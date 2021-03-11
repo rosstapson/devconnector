2d9571191acdebea29705e7dece5fee52bcca40d
Ross Tapson <Ross@wardandpartners.co.za>
2021-03-11 10:30:36 +0200

    various profile gets

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -29,6 +29,57 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
         })
 })
 
+router.get('/handle/:handle', (req, res) => {
+    const errors = {}
+    Profile.findOne({ handle: req.params.handle })
+        .populate('user', ['name', 'email', 'avatar'])
+        .then(profile => {
+            if(!profile) {
+                errors.noprofile = 'There is no profile for this user'
+                return res.status(404).json(errors)
+            }
+            res.json(profile)
+        })
+        .catch(err => {
+            res.status(404).json(err)
+        })
+})
+
+
+router.get('/user/:user_id', (req, res) => {
+    const errors = {}
+    
+    Profile.findOne({ user: req.params.user_id })
+        .populate('user', ['name', 'email', 'avatar'])
+        .then(profile => {
+            if(!profile) {
+                errors.noprofile = 'There is no profile for this user'
+                return res.status(404).json(errors)
+            }
+            res.json(profile)
+        })
+        .catch(err => {
+            res.status(404).json({profile: 'Invalid user id'})
+        })
+})
+
+router.get('/all', (req, res) => {
+    const errors = {}
+    
+    Profile.find()
+        .populate('user', ['name', 'email', 'avatar'])
+        .then(profiles => {
+            if(!profiles ) {
+                errors.noprofile = 'No profiles'
+                return res.status(404).json(errors)
+            }
+            res.json(profiles)
+        })
+        .catch(err => {
+            res.status(404).json({profile: 'Invalid user id'})
+        })
+})
+
 router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
     
     const { errors, isValid} = validateProfileInput(req.body)
