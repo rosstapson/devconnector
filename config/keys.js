544a6368469ab5ae1e3a98a255d8f48fb151cea0
Ross Tapson <Ross@wardandpartners.co.za>
2021-03-12 14:11:03 +0200

    post comments

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://ross:ross@cluster0.4bssa.mongodb.net/devconnector'
-    // mongoURI: 'mongodb+srv://ross:ross@cluster0.4bssa.mongodb.net/devconnector?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://ross:ross@cluster0.4bssa.mongodb.net/devconnector',
+    // mongoURI: 'mongodb+srv://ross:ross@cluster0.4bssa.mongodb.net/devconnector?retryWrites=true&w=majority',
+    secret: 'I drink and code things'
 }
